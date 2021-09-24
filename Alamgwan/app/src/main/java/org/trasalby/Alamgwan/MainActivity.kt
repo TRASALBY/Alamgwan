ac8a85b854fe2948513cd8dada7379bfc82edfd2
TRASALBY <ghtjd8264@naver.com>
2021-09-25 02:45:51 +0900

    jsoup 관련 자료 확인 필요 비동기 진행에 대한 이해

--- Alamgwan/app/src/main/java/org/trasalby/Alamgwan/MainActivity.kt
+++ Alamgwan/app/src/main/java/org/trasalby/Alamgwan/MainActivity.kt
@@ -1,11 +1,38 @@
 package org.trasalby.Alamgwan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.jsoup.Jsoup
+import kotlin.concurrent.thread
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var Breakfast : TextView
+    lateinit var Lunch : TextView
+    lateinit var Dinner : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Breakfast = findViewById(R.id.Breakfast)
+        Lunch = findViewById(R.id.Lunch)
+        Dinner = findViewById(R.id.Dinner)
+
+        Thread(Runnable {
+            val url = "https://naver.com"
+            val doc = Jsoup.connect(url).get()
+            var elements = doc.select("span.an_txt")
+
+
+            Log.d("qwer","qwer"+ "test")
+        })
+
+
+
     }
+
+
 }
