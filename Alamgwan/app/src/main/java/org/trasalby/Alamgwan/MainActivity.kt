f39187221e06f04f512eaed6c942f472e4539bdd
TRASALBY <ghtjd8264@naver.com>
2021-09-28 01:02:27 +0900

    코루틴 사용 크롤링 성공, 요일별 메뉴 구분 성공, 디자인 조정 필요

--- Alamgwan/app/src/main/java/org/trasalby/Alamgwan/MainActivity.kt
+++ Alamgwan/app/src/main/java/org/trasalby/Alamgwan/MainActivity.kt
@@ -8,13 +8,21 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jetbrains.anko.db.NULL
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import java.io.IOException
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
+import kotlin.collections.ArrayList
 
 //네트워크사용에서 비동기로 진행해야 한다는건 이해
 //비동기는 어떻게 사용해야 하는가
@@ -24,17 +32,15 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var Breakfast : TextView
-    lateinit var Lunch : TextView
-    lateinit var Dinner : TextView
-    lateinit var Week : TextView
-    lateinit var Previous : Button
-    lateinit var Next : Button
-    lateinit var Day : TextView
-    var week : String = ""
+    lateinit var Breakfast: TextView
+    lateinit var Lunch: TextView
+    lateinit var Dinner: TextView
+    lateinit var Week: TextView
+    lateinit var Previous: Button
+    lateinit var Next: Button
+    lateinit var Day: TextView
+    var week: String = ""
     val cal = Calendar.getInstance()
-
-    private val htmlPageUrl = "https://newgh.gnu.ac.kr/dorm/ad/fm/foodmenu/selectFoodMenuView.do"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,57 +56,78 @@ class MainActivity : AppCompatActivity() {
         Next = findViewById(R.id.next)
         cal.time = Date()
 
-        val df : DateFormat = SimpleDateFormat("MM월 dd일")
+        val df: DateFormat = SimpleDateFormat("MM월 dd일")
         Day.text = df.format(cal.time)
+        var meal_menuList : ArrayList<ArrayList<Element>>
 
-        val jsoupAsyncTask: JsoupAsyncTask = JsoupAsyncTask()
-        jsoupAsyncTask.execute()
-        while (true){
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.IO){
+                meal_menuList = crawlingMenu()
+            }
+            var weekNum = cal.get(Calendar.DAY_OF_WEEK)-1
+            setMenu(Breakfast, Lunch, Dinner, meal_menuList, weekNum)
             Next.setOnClickListener {
-                cal.add(Calendar.DATE,+1)
+                cal.add(Calendar.DATE, +1)
+                weekNum += 1
+                if(weekNum >7){
+                    weekNum %= 7
+                }
                 Day.text = df.format(cal.time)
+                setMenu(Breakfast, Lunch, Dinner, meal_menuList, weekNum)
             }
             Previous.setOnClickListener {
-                cal.add(Calendar.DATE,-1)
+                cal.add(Calendar.DATE, -1)
+                weekNum -= 1
+                if(weekNum < 0){
+                    weekNum + 7
+                }
                 Day.text = df.format(cal.time)
+                setMenu(Breakfast, Lunch, Dinner, meal_menuList, weekNum)
             }
-            Day.setOnClickListener{
+            Day.setOnClickListener {
                 cal.time = Date()
+                weekNum = cal.get(Calendar.DAY_OF_WEEK)-1
                 Day.text = df.format(cal.time)
-            }
-            if(!week.equals("")){
-                Week.text = week
-                break
+                setMenu(Breakfast, Lunch, Dinner, meal_menuList, weekNum)
             }
         }
     }
-    private inner class JsoupAsyncTask :
-        AsyncTask<Void?, Void?, Void?>() {
-        override fun onPreExecute() {
-            super.onPreExecute()
-        }
+}
 
-        protected override fun doInBackground(vararg p0: Void?): Void? {
-            try {
-                val doc: Document = Jsoup.connect(htmlPageUrl).get()
-                val week_day = doc.select("span.txt_p")
-                week = Html.fromHtml(week_day.toString()).toString();
+fun setMenu(Breakfast : TextView, Lunch : TextView, Dinner : TextView, meal_menuList : ArrayList<ArrayList<Element>>, weekNum : Int){
+
+    Breakfast.text = Html.fromHtml(meal_menuList[0][weekNum].toString())
+    Lunch.text = Html.fromHtml(meal_menuList[1][weekNum].toString())
+    Dinner.text = Html.fromHtml(meal_menuList[2][weekNum].toString())
+}
+
+fun crawlingMenu() : ArrayList<ArrayList<Element>>{
+
+    val URL : String = "https://newgh.gnu.ac.kr/dorm/ad/fm/foodmenu/selectFoodMenuView.do"
+    val doc : Document = Jsoup.connect(URL).get()
+    val elements : Elements = doc.select("div.BD_table.scroll_gr table tbody tr td")
+
+    var meal_menuList = ArrayList<ArrayList<Element>>()
+    var Break_menuList = ArrayList<Element>()
+    var Lunch_menuList = ArrayList<Element>()
+    var Dinner_menuList = ArrayList<Element>()
 
 
-
-                val elements = doc.select("div.foodLst_wrap").select("tr")[1]
-
-
-                Log.d("Alamgwan","test"+ elements.text() )
-            } catch (e: IOException) {
-                e.printStackTrace()
+    for (i in 0..2) {
+        for(j in 0..6){
+            if(i==0){
+                Break_menuList.add(elements[j])
             }
-            return null
-        }
-
-        override fun onPostExecute(result: Void?) {
-
+            if(i==1){
+                Lunch_menuList.add(elements[j+7])
+            }
+            else{
+                Dinner_menuList.add(elements[j+14])
+            }
         }
     }
-
+    meal_menuList.add(Break_menuList)
+    meal_menuList.add(Lunch_menuList)
+    meal_menuList.add(Dinner_menuList)
+    return meal_menuList
 }
